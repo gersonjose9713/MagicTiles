c07a13144dd09bf38683e72ff6209191f1bd4c9e
Gerson Almeida <gersonjose9713@gmail.com>
2023-02-04 04:57:12 -0300

    Organizacao e melhorias de performace

--- keys.py
+++ keys.py
@@ -1,9 +1,10 @@
 import ctypes
-import time
+import numpy as np
+sqrt3 = np.sqrt(3)
+sqrt5 = np.sqrt(5)
 
 SendInput = ctypes.windll.user32.SendInput
 
-# C struct redefinitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
@@ -34,24 +35,77 @@ class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
 
-# Actuals Functions
-
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+    cmd = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(cmd), ctypes.sizeof(cmd))
 
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
     ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
-    x = Input( ctypes.c_ulong(1), ii_ )
-    ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+    cmd = Input( ctypes.c_ulong(1), ii_ )
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(cmd), ctypes.sizeof(cmd))
 
-if __name__ == '__main__':
-    PressKey(0x11)
-    time.sleep(1)
-    ReleaseKey(0x11)
-    time.sleep(1)
+def MousePosition():
+    extra = MouseInput()
+    ctypes.windll.user32.GetCursorPos(ctypes.pointer(extra))
+    return  extra.dx,extra.dy
+
+
+def MoveMouse(x, y):
+    window_x, window_y = ctypes.windll.user32.GetSystemMetrics(0), ctypes.windll.user32.GetSystemMetrics(1)
+    x = 1 + int(x * 65536/window_x)
+    y = 1 + int(y * 65536/window_y)
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.mi = MouseInput(x, y, 0, (0x0001 | 0x8000), 0, ctypes.pointer(extra))
+    cmd = Input(ctypes.c_ulong(0), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(cmd), ctypes.sizeof(cmd))
+
+def LeftClick():
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.mi = MouseInput(0, 0, 0, 0x0002, 0, ctypes.pointer(extra))
+    cmd = Input(ctypes.c_ulong(0), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(cmd), ctypes.sizeof(cmd))
+
+def LeftRelease():
+    extra = ctypes.c_ulong(0)
+    ii_ = Input_I()
+    ii_.mi = MouseInput(0, 0, 0, 0x0004, 0, ctypes.pointer(extra))
+    cmd = Input(ctypes.c_ulong(0), ii_)
+    ctypes.windll.user32.SendInput(1, ctypes.pointer(cmd), ctypes.sizeof(cmd))
+
+def WindMouse(dest_x, dest_y, G_0=9, W_0=3, M_0=15, D_0=12, move_mouse=MoveMouse):
+    start_x,start_y = MousePosition()
+    current_x,current_y = start_x,start_y
+    v_x = v_y = W_x = W_y = 0
+    while (dist:=np.hypot(dest_x-start_x,dest_y-start_y)) >= 1:
+        W_mag = min(W_0, dist)
+        if dist >= D_0:
+            W_x = W_x/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+            W_y = W_y/sqrt3 + (2*np.random.random()-1)*W_mag/sqrt5
+        else:
+            W_x /= sqrt3
+            W_y /= sqrt3
+            if M_0 < 3:
+                M_0 = np.random.random()*3 + 3
+            else:
+                M_0 /= sqrt5
+        v_x += W_x + G_0*(dest_x-start_x)/dist
+        v_y += W_y + G_0*(dest_y-start_y)/dist
+        v_mag = np.hypot(v_x, v_y)
+        if v_mag > M_0:
+            v_clip = M_0/2 + np.random.random()*M_0/2
+            v_x = (v_x/v_mag) * v_clip
+            v_y = (v_y/v_mag) * v_clip
+        start_x += v_x
+        start_y += v_y
+        move_x = int(np.round(start_x))
+        move_y = int(np.round(start_y))
+        if current_x != move_x or current_y != move_y:
+            move_mouse(current_x:=move_x,current_y:=move_y)
+    return current_x,current_y
